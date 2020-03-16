6398f8b251bf7a6aa1bb53d866842a5ea1824b91
Nikita Davidovski <nikita.davidouski@gmail.com>
2020-03-16 15:04:55 +0300

    Adding project

---
+++ task5/project/src/Check.java
@@ -0,0 +1,18 @@
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.io.PrintWriter;
+
+@WebServlet(name = "Check")
+public class Check extends HttpServlet {
+
+    protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        response.setContentType("application/json");
+        PrintWriter out = response.getWriter();
+        out.print("{\"success\":true}");
+        out.flush();
+    }
+}
